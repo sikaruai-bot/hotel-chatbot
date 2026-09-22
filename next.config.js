6266b787d0e83c7308c9fb08bc46636abe16554c
Mingma Sherpa <info@hotelsherpasoul.com>
2026-09-22 18:19:05 +0545

    fix(db): include seeded prisma/dev.db and outputFileTracingIncludes for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/dev.db'],
+    },
+  },
   images: {
     domains: ['images.unsplash.com'],
   },
